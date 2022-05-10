88fccc72a08ca8c632e0638e63e9c6acbcce363d
TomasGutierrezO <97989125+TomasGutierrezO@users.noreply.github.com>
2022-05-10 16:53:38 -0500

    "First Commit"

---
+++ src/main/java/com/mycompany/calcuador_de_dias/Calcuador_de_dias.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calcuador_de_dias;
+
+/**
+ *
+ * @author tomas
+ */
+public class Calcuador_de_dias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
